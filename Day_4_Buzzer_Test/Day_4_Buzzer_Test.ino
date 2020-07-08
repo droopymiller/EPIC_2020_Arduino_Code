16988a8a2a30ffa9cc9adb693e7fe50c6958253d
Drew Miller <amill118@calpoly.edu>
2020-07-08 12:59:29 -0700

    Added buzzer test

---
+++ Day_4_Buzzer_Test/Day_4_Buzzer_Test.ino
@@ -0,0 +1,24 @@
+//www.elegoo.com
+//2016.12.08
+// This is a simple program to make sure the passive buzzer works
+// The passive buzzer positive terminal should go to the arduino uno pin 8
+// The passive buzzer negative termal should go to GND on the arduino
+// The passive buzzer will play two notes just to verify that everything is working.
+
+#include "pitches.h"
+ 
+// notes in the melody:
+int melody[] = {300, 400};
+int duration = 500;  // 500 miliseconds
+ 
+void setup() {
+ 
+}
+ 
+void loop() {  
+    tone(8, melody[1], duration);
+    delay(500);
+    tone(8, melody[2], duration);
+    delay(500);
+
+}
